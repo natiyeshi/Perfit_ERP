10355ec21e0f13b8446892fbef55e94523573e1e
Natnael Yeshiwas <natiyeshi16@gmail.com>
2025-02-01 16:58:53 +0300

    AsA

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -4,14 +4,7 @@ const localApi = "http://localhost:4040/api/v1/"
 const mainApi = "https://perfit-erp.onrender.com/api/v1/"
 
 const instance = axios.create({
-  baseURL: mainApi,
-  // headers: {
-  //     'Content-Type': 'application/json',
-  //     'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type',
-  //     'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS',
-  //     'Access-Control-Allow-Origin': 'http://localhost:3000',
-  //     'Access-Control-Allow-Credentials' : true,
-  //   },
+  baseURL: localApi,
   withCredentials: true,
 });
 
