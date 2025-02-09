1c98a6c4604cecfc7acf79466bfd66868cea7c0b
Natnael Yeshiwas <natiyeshi16@gmail.com>
2025-02-09 13:14:50 +0300

    test 4 for ci-cd

--- server/src/middleware/rate-limiter.ts
+++ server/src/middleware/rate-limiter.ts
@@ -1,6 +1,5 @@
 import { rateLimit } from "express-rate-limit";
 
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 1500, // Limit each IP to 1500 requests per `window`.
